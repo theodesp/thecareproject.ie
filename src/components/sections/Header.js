db7949c0fe913372480144031535c01baf730022
Theo <theo.despoudis@teckro.com>
2019-09-07 19:36:03 +0100

    Updates

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -15,7 +15,7 @@ const Header = () => (
         ) {
           childImageSharp {
             fluid(maxWidth: 1400) {
-              ...GatsbyImageSharpFluid_withWebp_tracedSVG
+              ...GatsbyImageSharpFluid
             }
           }
         }
